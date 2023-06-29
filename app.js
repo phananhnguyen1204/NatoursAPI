42df3cce4b0ab345b2045faa8c7a488ad7ee5804
Phan Anh Nguyễn <phananhnguyen@Phans-MacBook-Pro.local>
2023-06-28 20:34:36 -0400

    create server.js

--- app.js
+++ app.js
@@ -37,7 +37,4 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App is running on port ${port}`);
-});
+module.exports = app;
