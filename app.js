be661761b851b940591c590642bd6c942ccbf107
Phan Anh Nguyễn <phananhnguyen@Phans-MacBook-Pro.local>
2023-06-28 20:23:01 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const morgan = require('morgan');
+const express = require('express');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+//1) MIDDLEWARES
+
+const app = express();
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from middleware');
+  next();
+});
+
+// app.get('/', (req, res) => {
+//   // res.status(200).send('Hello from server side!');
+//   res.status(200).json({ message: 'Hello from server side!', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint....');
+// });
+
+// app.get('/api/v1/tours', getAllTours);
+
+// app.get('/api/v1/tours/:id', getTour);
+
+// app.post('/api/v1/tours', createTour);
+
+// app.patch('/api/v1/tours/:id', updateTour);
+
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+//3) Routes
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
---
+++ controllers/tourController.js
@@ -0,0 +1,81 @@
+const fs = require('fs');
+
+let tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+exports.getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: tours,
+  });
+};
+exports.getTour = (req, res) => {
+  // console.log(req.params);
+  const id = req.params.id * 1;
+  // console.log(id);
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID',
+    });
+  }
+  const tour = tours.find((el) => el.id === id);
+  res.status(200).json({
+    status: 'success',
+    tour,
+  });
+};
+
+exports.createTour = (req, res) => {
+  console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  let newTour = { id: newId, ...req.body };
+  tours = [...tours, newTour];
+  //need to be asynchrouns not to block the code
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+
+exports.updateTour = (req, res) => {
+  const id = req.params.id * 1;
+  // console.log(id);
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour>',
+    },
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  const id = req.params.id * 1;
+  // console.log(id);
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
